8044c19feeabb9e2c5e22855a7bd9b74d9de7391
japh <japh@localhost.localdomain>
2023-02-14 18:57:02 -0500

    Estructura inicial del proyecto atomi desing

---
+++ src/Components/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@mui/material"
+
+function App() {
+
+  return (
+    <div className="App">
+      Hola mundo!
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  )
+}
+
+export default App
